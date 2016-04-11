01d413ebdf042fe43ae2cddfa755bbf175510427
tgsmith61591 <tgsmith61591@gmail.com>
2016-04-11 17:02:38 -0500

    Added test for CoreRestrictiveChunkingStrategy

--- src/test/java/com/clust4j/algo/ParallelTaskTests.java
+++ src/test/java/com/clust4j/algo/ParallelTaskTests.java
@@ -1,15 +1,10 @@
 package com.clust4j.algo;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
 import org.junit.Test;
 
-import com.clust4j.GlobalState;
-import com.clust4j.algo.ParallelChunkingTask.Chunk;
 import com.clust4j.algo.ParallelChunkingTask.ChunkingStrategy;
 import com.clust4j.algo.ParallelChunkingTask.SimpleChunkingStrategy;
+import com.clust4j.algo.ParallelChunkingTask.CoreRestrictiveChunkingStrategy;
 import com.clust4j.utils.MatUtils;
 
 public class ParallelTaskTests {
@@ -18,10 +13,10 @@ public class ParallelTaskTests {
 	public void test() {
 		double[][] X = MatUtils.randomGaussian(750, 2);
 		ChunkingStrategy strat = new SimpleChunkingStrategy();
-		ArrayList<Chunk> chunks = strat.map(X);
-		assertTrue(chunks.size() == GlobalState.ParallelismConf.NUM_CORES);
-		assertTrue(strat.getNumChunks(X) == GlobalState.ParallelismConf.NUM_CORES);
-		assertTrue(ChunkingStrategy.getChunk(X, 500, 1).size() == 250);
+		strat.map(X); // want to make sure it works.
+		
+		strat = new CoreRestrictiveChunkingStrategy(1);
+		strat.map(X);
 	}
 
 }
